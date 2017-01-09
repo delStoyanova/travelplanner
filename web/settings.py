8613a04aab6c805866f9a4b6539f680f51c5bbc1
travelplannerStudent <d.stoyanova@student.tue.nl>
2017-01-09 15:19:07 +0200

    still fixing3

--- web/settings.py
+++ web/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT ="/home/holidayplanner/travelplanner/media/avatars/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "/media/avatars/"
+MEDIA_URL = "/media/"

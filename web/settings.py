26b095c8aef31d7fb7e80deafeccd7caaf36c767
travelplannerStudent <d.stoyanova@student.tue.nl>
2017-01-09 14:20:37 +0200

    change in url

--- web/settings.py
+++ web/settings.py
@@ -133,5 +133,5 @@ MEDIA_ROOT ='/home/holidayplanner/travelplanner/media/avatars'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://holidayplanner/travelplanner/media/avatars'
+MEDIA_URL = '/media/avatar'
 

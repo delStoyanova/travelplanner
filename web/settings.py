e66e8ff646fe240285a39047b6de1bfd64ab1473
travelplannerStudent <d.stoyanova@student.tue.nl>
2017-01-09 15:07:45 +0200

    still fixing0

--- web/settings.py
+++ web/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/avatar/'

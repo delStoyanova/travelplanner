6efd7d47d6527b8c1f68e6dd028f6be8171b9515
travelplannerStudent <d.stoyanova@student.tue.nl>
2017-01-09 13:58:05 +0200

    changing media root

--- web/settings.py
+++ web/settings.py
@@ -128,9 +128,10 @@ STATICFILES_DIRS = [
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home2/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT ='/home/holidayplanner/travelplanner/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
+
